943f28844b99cafdc49668d606a5c8877c345626
Lachu Tirumalai <lachu.tirumalai@gmail.com>
2023-01-08 12:21:45 -0600

    Update streamlit_app.py
    Formatting the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
-
 streamlit.title('My Mom''s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -15,5 +11,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 

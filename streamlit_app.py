4f6f45d527f551201183ee4acb8c2d5283663cf9
Lachu Tirumalai <lachu.tirumalai@gmail.com>
2023-01-08 12:05:26 -0600

    Update streamlit_app.py
    Updated the Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')

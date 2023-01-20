f52aa8eacf49b0b2ddc251edf649056f07cddbfb
Lachu Tirumalai <lachu.tirumalai@gmail.com>
2023-01-19 18:33:56 -0600

    Update streamlit_app.py
    Add a text box to enter a fruit name and add it to the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 
 import streamlit
 import pandas
+import requests
+
 
 streamlit.title('My Mom''s New Healthy Diner')
 
@@ -22,14 +24,18 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+
 # New section to display fruitvice api response
 streamlit.header('Fruitvice Fruit Advice!')
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+#lets add a text input
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Parse the JSON and read the text 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display the data in a tabular format
 streamlit.dataframe(fruityvice_normalized)
-
